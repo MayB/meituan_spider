50e5b1061800b2850baa5adfe5296f57dd77576c
Alan <alan@AlandeMacBook-Pro.local>
2015-07-17 14:00:50 +0800

    first

---
+++ meituan_spider/meituan_spider/spiders/category_spider.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import scrapy
+import urllib
+import logging
+import time
+import os
+import sys
+import re
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+class CategorySpider(scrapy.Spider):
+  name = "category_spider"
+  def __init__(self):
+    super(CategorySpider, self).__init__()
+    self.allowed_domains = ["i.meituan.com"]
+    url_list = []
+    #url_list.append("http://i.meituan.com/guangzhou?cid=1&stid=_b1&cateType=poi&p=")
+    for i in xrange(26):
+      url_list.append("http://i.meituan.com/index/changecity/more/" + chr(i + ord('A')) + "?cevent=imt%2FselectCity%2Fmore")
+    self.start_urls = tuple(url_list)
+
+  def parse(self, response):
+    city_list = response.selector.xpath('//ul[@class="table box nopadding"]/li/a/@data-citypinyin').extract()
+    for city in city_list:
+      url = "http://i.meituan.com/" + city + "?cid=1&stid=_b1&cateType=poi&p="
+      for i in range(1, 1000):
+        yield scrapy.http.Request(url=url + str(i), callback=self.ParseItemList)
+
+  def ParseItemList(self, response):
+    main_list = response.selector.xpath('//div[@class="deal-container"]/div[@id="deals"]/dl[@class="list"]')
+    for item in main_list:
+      title = item.xpath('dd[@class="poi-list-item"]/a/span[@class="poiname"]/text()').extract()
+      title = ''.join(title)
+      sub_item_list = item.xpath('dd/dl[@class="list"]/dd/a/div[@class="dealcard dealcard-poi"]/div[@class="dealcard-block-right"]/div[@class="title text-block"]/text()').extract()
+      for sub_item in sub_item_list:
+        print title + " " + sub_item
+
