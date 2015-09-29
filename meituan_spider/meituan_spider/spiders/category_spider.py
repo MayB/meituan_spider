78388b20c12a8de993d44da3e07830af6bc7f120
MayB <1074439210@qq.com>
2015-09-29 16:33:29 +0800

    添加写数据库方法

--- meituan_spider/meituan_spider/spiders/category_spider.py
+++ meituan_spider/meituan_spider/spiders/category_spider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 import scrapy
+import MySQLdb
 import urllib
 import logging
 import time
@@ -37,4 +38,14 @@ class CategorySpider(scrapy.Spider):
       sub_item_list = item.xpath('dd/dl[@class="list"]/dd/a/div[@class="dealcard dealcard-poi"]/div[@class="dealcard-block-right"]/div[@class="title text-block"]/text()').extract()
       for sub_item in sub_item_list:
         print title + " " + sub_item
-
+  def write2DB(self, sub_item)
+    try:
+        conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='test',port=3306)
+        cur=conn.cursor()
+        #cur.execute('select * from user')
+        value = [1, 'hello world!']
+        cur.execute('insert into test values(%s,%s)',value)
+        cur.close()
+        conn.close()
+    except MySQLdb.Error,e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
